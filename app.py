29679389f83e8fd5c18257451233916ba59c4db5
DavideAmI <davide.itri@gmail.com>
2026-03-14 16:21:55 -0400

    Payload Match

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
+# IMPORTANT: This must match EXACTLY what you enter in the eBay portal
 VERIFICATION_TOKEN = "D4V3-PR0J3CT-C0M1C-D4T4-H1ST0RY-TR4CK1NG-2026"
 
 @app.get("/")
